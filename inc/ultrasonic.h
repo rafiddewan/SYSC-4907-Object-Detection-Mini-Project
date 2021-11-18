5b691b9600d5ad4c3e7175633748eda67aaa2f95
NickC99 <nickcoutts99@gmail.com>
2021-11-18 13:35:26 -0500

    Inital configs for ultrasonic

---
+++ inc/ultrasonic.h
@@ -0,0 +1,13 @@
+#include <MKL25Z4.h>
+
+#define PIN_TRIG_PORT					PORTA
+#define PIN_TRIG_PT						PTA
+#define PIN_TRIG_SHIFT				( 17 )
+#define PIN_TRIG                    (1 << PIN_TRIG_SHIFT)
+
+#define PIN_ECHO_PORT						PORTA
+#define PIN_ECHO_PT							PTA
+#define PIN_ECHO_SHIFT						( 16 )
+#define PIN_ECHO                 ( 1 << PIN_ECHO_SHIFT)
+
+void Init_Ultrasonic(void);
